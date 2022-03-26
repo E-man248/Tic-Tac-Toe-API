07ea678dcd913675328ff4c29a328d4e0978e1da
E-man248 <67338595+E-man248@users.noreply.github.com>
2022-03-26 14:35:00 -0600

    Created Data Folder and Game Repository Interface
    Created a Data folder for storing object repository references and functions.
    
    The Game Repository Interface was added to this folder which allows for loose coupling of classes with the interfaces methods and enforces the interface methods used by those who implement them.
    
    A Default Game Repository class was added to the Data folder implementing the Game Repository interface. This is primarily for testing and seeing basic implementation of the base functions of the interface.

---
+++ Data/DefaultGameRepository.cs
@@ -0,0 +1,18 @@
+using TicTacToeAPI.Models;
+
+namespace TicTacToeAPI.Data
+{
+    /** <summary>
+        This class models a default implementation of a game repository. This
+        is primarily used for testing functionality of the repository with safe
+        values. This class is not intended for full product implementation.
+    </summary> **/
+    public class DefaultGameRepository : GameRepositoryInterface
+    {
+        public Game PostNewGame()
+        {
+            int[] defaultIDs = {0, 1};
+            return new Game{gameID = 0, status = 0, playerIDs = defaultIDs };
+        }
+    }
+}
---
+++ Data/GameRepositoryInterface.cs
@@ -0,0 +1,20 @@
+using TicTacToeAPI.Models;
+
+namespace TicTacToeAPI.Data
+{
+    /** <summary>
+        This interface enforces the methods that should be implemented in
+        a Game Repository so that classes implementing this interface are
+        implement these methods.
+    </summary> **/
+    public interface GameRepositoryInterface
+    {
+        /** <summary>
+            This method creates a game and saves that game to the repository.
+        </summary>
+        <returns>
+            Newly created instance of the game that was saved.
+        </returns>**/
+        Game PostNewGame();
+    }
+}
